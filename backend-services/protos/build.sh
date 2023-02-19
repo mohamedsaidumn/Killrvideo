989b7333a005ee82f8a5fe919b49b369d3daa8da
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-02-19 11:36:29 -0600

    put the generated files under source directories and gitignored google files

--- backend-services/protos/build.sh
+++ backend-services/protos/build.sh
@@ -1,38 +1,45 @@
 # why does PROTO_DIR work for poth output directores ????????
-PROTO_DIR=./protos/generated
+GEN_PROTO_DIR=./protos/generated
+COMMENTS_DIR=comments
+COMMON_DIR=common
+GOOGLE_API_DIR=google/api
 
 
-# Generate JavaScript code
+
+# # Generate JavaScript code
+# yarn run grpc_tools_node_protoc \
+#     --js_out=import_style=commonjs,binary:${GEN_PROTO_DIR} \
+#     --grpc_out=grpc_js:${GEN_PROTO_DIR} \
+#     --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
+#     -I ./protos \
+#     protos/*.proto
+
+# The I is the relative path for all my imports
+# Generate TypeScript and js code for my proto code (d.ts) and grpc servce stub if applicable.
 yarn run grpc_tools_node_protoc \
-    --js_out=import_style=commonjs,binary:${PROTO_DIR} \
-    --grpc_out=grpc_js:${PROTO_DIR} \
+    --js_out=import_style=commonjs,binary:${GEN_PROTO_DIR} \
+    --grpc_out=grpc_js:${GEN_PROTO_DIR} \
     --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
-    -I ./protos \
-    protos/*.proto
-
-# Generate TypeScript code (d.ts)
-yarn run grpc_tools_node_protoc \
+    --ts_out=grpc_js:${GEN_PROTO_DIR} \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-    --ts_out=grpc_js:${PROTO_DIR} \
-    --descriptor_set_out=${PROTO_DIR}/killrvideo_proto_descriptor.pb\
+    --descriptor_set_out=${GEN_PROTO_DIR}/killrvideo_proto_descriptor.pb\
     -I ./protos \
-    --include_imports protos/*.proto
+    --include_imports protos/${COMMENTS_DIR}/*.proto protos/${COMMON_DIR}/*.proto
+
+# Generate proto js and ts code for google api's protos
+yarn run grpc_tools_node_protoc \
+    --js_out=import_style=commonjs,binary:${GEN_PROTO_DIR} \
+    --ts_out=${GEN_PROTO_DIR} \
+    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+    --proto_path=./protos \
+    protos/${GOOGLE_API_DIR}/*.proto
 
 
 #ANA_DIR=./proto/generated/google/api
 #why does the ANA_DIR not the way to do it and PROTO_DIR is the way
 #what is binary
 
-# Generate JavaScript code
-yarn run grpc_tools_node_protoc \
-    --js_out=import_style=commonjs,binary:${PROTO_DIR} \
-    --proto_path=./protos \
-    protos/google/api/*.proto
+cp -r protos/generated src/
 
-# Generate TypeScript code (d.ts)
-yarn run grpc_tools_node_protoc \
-    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-    --ts_out=${PROTO_DIR} \
-    --proto_path=./protos\
-    protos/google/api/*.proto
+
 
